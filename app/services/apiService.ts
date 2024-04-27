108caa6b77dc4c2d32610fdd7cff86f5e4c64b77
Maxi Ovelar <73977645+maxiovelar@users.noreply.github.com>
2024-04-27 21:34:35 +0200

    Remove comments

--- app/services/apiService.ts
+++ app/services/apiService.ts
@@ -3,7 +3,6 @@ const { NEXT_PUBLIC_API_BASE_URL } = process.env;
 export const getRangeValues = async (endpoint: string) => {
     const res = await fetch(NEXT_PUBLIC_API_BASE_URL + endpoint);
     if (!res.ok) {
-        // This will activate the closest `error.js` Error Boundary
         throw new Error("Failed to fetch data");
     }
 

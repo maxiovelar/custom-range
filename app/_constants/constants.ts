c0d4fabe8ecbf25e3c848dfa438a5ee236ce35d3
Maxi Ovelar <73977645+maxiovelar@users.noreply.github.com>
2024-04-27 14:29:18 +0200

    Add constants file and types file

---
+++ app/_constants/constants.ts
@@ -0,0 +1,30 @@
+import { type Page, type Link } from "../_types/types"
+
+export const links: Record<string, Link> = {
+    home: {
+        href: "/",
+        text: "Home",
+    },
+    menu1: {
+        href: "/exercise1",
+        text: "Exercise 1",
+    },
+    menu2: {
+        href: "/exercise2",
+        text: "Exercise 2",
+    },
+    github: {
+        href: "https://github.com/maxiovelar",
+        text: "Maximiliano Ovelar",
+    },
+    nextjs: {
+        href: "https://nextjs.org/",
+        text: "Next.js",
+    },
+};
+
+export const pageTitles: Record<Page, string> = {
+    home: "Custom Ranges Exercise",
+    exercise1: "Normal Range",
+    exercise2: "Fixed Values Range",
+};
---
+++ app/_types/types.ts
@@ -0,0 +1,7 @@
+export interface Link {
+    href: string;
+    text: string;
+}
+
+export type Page = "home" | "exercise1" | "exercise2";
+

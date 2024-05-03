0513cf1a6a26fca1a0d72439a12ae3cbf8962339
Maxi Ovelar <73977645+maxiovelar@users.noreply.github.com>
2024-05-03 02:38:53 +0200

    Test UI components

--- app/_common/components/navbar/Navbar.tsx
+++ app/_common/components/navbar/Navbar.tsx
@@ -1,26 +1,22 @@
-import Link from "next/link";
-import React from "react";
-import { links } from "@/_common/constants/constants";
-import styles from "./Navbar.module.scss";
+import Link from 'next/link';
+import React from 'react';
+import { links } from '@/_common/constants/constants';
+import styles from './Navbar.module.scss';
 
 export const Navbar = () => {
-  return (
-    <nav className={styles.nav}>
-      <Link href={links.home.href} className={styles.link}>
-        <p>{links.home.text}</p>
-      </Link>
+    return (
+        <nav className={styles.nav}>
+            <Link href={links.home.href} className={styles.link}>
+                {links.home.text}
+            </Link>
 
-      <Link href={links.menu1.href} className={styles.link}>
-        <p>
-          {links.menu1.text} <span>-&gt;</span>
-        </p>
-      </Link>
+            <Link href={links.menu1.href} className={styles.link}>
+                {links.menu1.text} <span>-&gt;</span>
+            </Link>
 
-      <Link href={links.menu2.href} className={styles.link}>
-        <p>
-          {links.menu2.text} <span>-&gt;</span>
-        </p>
-      </Link>
-    </nav>
-  );
+            <Link href={links.menu2.href} className={styles.link}>
+                {links.menu2.text} <span>-&gt;</span>
+            </Link>
+        </nav>
+    );
 };
